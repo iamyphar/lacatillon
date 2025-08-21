a44eeaf056279f9f749f815cf67e8f25591c10c9
raphaeldoutaz <raphaeldoutaz@gmail.com>
2025-08-21 17:42:17 +0200

    feat: desktop version

--- src/assets/js/photoswipe.js
+++ src/assets/js/photoswipe.js
@@ -2,7 +2,7 @@ import PhotoSwipeLightbox from 'photoswipe/lightbox'
 
 document.addEventListener('DOMContentLoaded', () => {
     const lightbox = new PhotoSwipeLightbox({
-        gallery: '#gallery--getting-started',
+        gallery: '#gallery',
         children: 'a',
         pswpModule: () => import('photoswipe')
     })
